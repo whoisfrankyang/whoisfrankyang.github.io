4fffa586e0d528a9c7375eeddf6129dd23ab20fe
whoisfrankyang <whoisfrankyang@gmail.com>
2026-02-26 11:17:48 -0500

    update

--- _pages/about.md
+++ _pages/about.md
@@ -6,7 +6,7 @@ permalink: /
 profile:
   align: right
   image: prof_pic.png
-  image_circular: false # crops the image to make it circular
+  image_circular: true # crops the image to make it circular
   more_info:
 
 selected_papers: true # includes a list of papers marked as "selected={true}"
--- _site/assets/js/cookie-consent-setup.js
+++ _site/assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="http://localhost:4000/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="http://localhost:4000/#contact">contact us</a>.'
               }
             ]
           }
